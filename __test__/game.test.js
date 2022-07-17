82faf0aa64ffb9fa77490ad3c4b73e986b48d41c
Noam Daloya <90097801+nananoam123@users.noreply.github.com>
2022-07-17 20:12:16 +0300

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,7 +1,7 @@
 const Game = require('../src/game').default
-// const fs = require('fs')
+/* const fs = require('fs')
 
-// describe('App', () => {
+ describe('App', () => {
 //  it('Contains the compiled JavaScript', async (done) => {
  //   fs.readFile('./public/main.js', 'utf8', (err, data) => {
  //    expect(err).toBe(null)
@@ -10,7 +10,7 @@ const Game = require('../src/game').default
   //  })
  // })
 //})
-//
+*/
 describe('Game', () => {
   let game, p1, p2
   beforeEach(() => {
